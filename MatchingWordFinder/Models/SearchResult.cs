b7faf5aab00b4b07535e2f62cbaa1ed89497cef6
Marius Monkam <cloudmonde@yahoo.com>
2023-06-25 15:18:03 +0300

    SeachResult class

---
+++ MatchingWordFinder/Models/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using MatchingWordFinder.Models;
+
+namespace MatchingWordFinder
+{
+    public class SearchResult
+    {
+        public List<MatchingWord> MatchingWords { get; set; }
+        public int Count { get; set; }
+        public string HighlightedParagraph { get; set; }
+    }
+}
